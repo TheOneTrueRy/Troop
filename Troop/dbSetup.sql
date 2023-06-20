842b010b8d9051248a932d6ec81953830fde2c35
Ry <OfficiallyRy@outlook.com>
2023-06-20 12:51:00 -0600

    Wrote the full dbSetup for tables needed for backend

--- Troop/dbSetup.sql
+++ Troop/dbSetup.sql
@@ -6,4 +6,41 @@ CREATE TABLE IF NOT EXISTS accounts(
   name varchar(255) COMMENT 'User Name',
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+) default charset utf8mb4 COMMENT '';
+
+-- EVENTS --
+CREATE TABLE IF NOT EXISTS events(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  creatorId VARCHAR(255) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  description VARCHAR(2000) DEFAULT 'No Description Given.',
+  coverImg VARCHAR(500) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  capacity INT NOT NULL,
+  startDate DATE NOT NULL,
+  isCanceled BOOLEAN DEFAULT false,
+  type VARCHAR(40) NOT NULL,
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8mb4 COMMENT '';
+
+-- TICKETS --
+CREATE TABLE IF NOT EXISTS tickets(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  eventId INT NOT NULL,
+  accountId VARCHAR(255) NOT NULL,
+
+  FOREIGN KEY (eventId) REFERENCES events(id) ON DELETE CASCADE,
+  FOREIGN KEY (accountId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8mb4 COMMENT '';
+
+-- COMMENTS --
+CREATE TABLE IF NOT EXISTS comments(
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  creatorId VARCHAR(255) NOT NULL,
+  eventId INT NOT NULL,
+  body VARCHAR(2000),
+
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
+  FOREIGN KEY (eventId) REFERENCES events(id) ON DELETE CASCADE
+) default charset utf8mb4 COMMENT '';
