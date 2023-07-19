5e573ab463f4b98e03a53e48595473bfac320f4a
Ry <OfficiallyRy@outlook.com>
2023-07-19 16:40:00 -0600

    Added that top picture and words to the home page

--- Troop.client/src/pages/HomePage.jsx
+++ Troop.client/src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
+import { observer } from "mobx-react-lite";
 import React, { useEffect } from "react";
 
-export default function HomePage() {
+function HomePage() {
 
   useEffect(() => {
     document.title = 'Troop - Home 🏠'
   }, [])
 
   return (
-    <div className="home-page">
-
+    <div className="home-page px-4">
+      <div className="row rounded elevation-1 home-header-pic">
+        <div className="col-4 d-flex flex-column justify-content-center">
+          <span className="fs-3 text-light py-1 text-shadow">
+            Get ahead of the scalpers.
+          </span>
+          <span className="fs-3 text-light py-1 text-shadow">
+            Reserve your seats now.
+          </span>
+          <span className="fs-3 text-light py-1 text-shadow">
+            Real events for real people.
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
+
+export default observer(HomePage)
