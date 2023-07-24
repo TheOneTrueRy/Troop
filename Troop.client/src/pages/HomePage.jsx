921b4ca879a3d073d5c0d0029eb8a343de652d26
Ry <OfficiallyRy@outlook.com>
2023-07-24 15:07:25 -0600

    Added row of category buttons, styled them, and cleaned up main scss

--- Troop.client/src/pages/HomePage.jsx
+++ Troop.client/src/pages/HomePage.jsx
@@ -23,8 +23,39 @@ function HomePage() {
         </div>
       </div>
       <div className="row">
-        <div className="col-12 g-0">
-          <span className="fs-1">Test</span>
+        <div className="col-12 pt-3">
+          <div className="row rounded bg-dark bg-gradient elevation-2">
+            <div className={"col-4 col-lg-2 d-flex justify-content-center align-items-center py-3 selectable all"}>
+              <span className="fs-4 text-shadow">
+                All
+              </span>
+            </div>
+            <div className={"col-4 col-lg-2 d-flex justify-content-center align-items-center py-3 selectable"}>
+              <span className="fs-4 text-shadow">
+                Conventions
+              </span>
+            </div>
+            <div className={"col-4 col-lg-2 d-flex justify-content-center align-items-center py-3 selectable sports"}>
+              <span className="fs-4 text-shadow">
+                Sports
+              </span>
+            </div>
+            <div className={"col-4 col-lg-2 d-flex justify-content-center align-items-center py-3 selectable digital"}>
+              <span className="fs-4 text-shadow">
+                Digital
+              </span>
+            </div>
+            <div className={"col-4 col-lg-2 d-flex justify-content-center align-items-center py-3 selectable"}>
+              <span className="fs-4 text-shadow">
+                Concerts
+              </span>
+            </div>
+            <div className={"col-4 col-lg-2 d-flex justify-content-center align-items-center py-3 selectable other"}>
+              <span className="fs-4 text-shadow">
+                Other
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
