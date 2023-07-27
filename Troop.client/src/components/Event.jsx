f69ca03b870f6de09a7b48efd549f6f6a13a9184
Ry <OfficiallyRy@outlook.com>
2023-07-26 19:34:34 -0600

    Added Canceled/At Capacity EventCard conditions, with custom styling

--- Troop.client/src/components/Event.jsx
+++ Troop.client/src/components/Event.jsx
@@ -7,22 +7,28 @@ function Event({ event }) {
 
   return (
 
-    <div className="col-12 col-md-6 col-lg-4 col-xl-3 px-3 py-3">
+    <div className="col-12 col-md-6 col-lg-4 col-xl-3 px-3 py-2">
       <Link to={`events/${event.id}`} title={`Visit the details page of "${event.name}"!`}>
         <div className="event-card selectable" style={{ backgroundImage: `url(${event.coverImg})` }}>
-          <div className="w-100 px-1 d-flex flex-column bg-blur">
-            <span className="fs-5 text-light text-shadow">
+          <div className="w-100 d-flex flex-column bg-blur bottom-event-card-rounded">
+            <span className="fs-5 text-light text-shadow ps-1">
               {event.name}
             </span>
-            <span className="text-light text-shadow">
+            <span className="text-light text-shadow ps-1">
               {event.location}
             </span>
-            <span className="text-light text-shadow">
+            <span className="text-light text-shadow ps-1">
               {event.startDate}
             </span>
-            <span className="text-light text-shadow text-end">
+            {event.capacity != 0 && event.isCanceled == false && <span className="text-light text-shadow text-end pe-1">
               {event.capacity} spots left
-            </span>
+            </span>}
+            {event.capacity == 0 && event.isCanceled == false && <span className="text-center w-100 fw-bold canceled">
+              At Capacity
+            </span>}
+            {event.isCanceled == true && <span className="text-center w-100 fw-bold canceled">
+              Canceled
+            </span>}
           </div>
         </div>
       </Link>
