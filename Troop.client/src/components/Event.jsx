81c3d03a166f36fefb62448cd52605e245297615
Ry <OfficiallyRy@outlook.com>
2023-07-26 19:18:59 -0600

    EventCards now have the coverImg background, name, location, startDate, capacity, and a nice blur

--- Troop.client/src/components/Event.jsx
+++ Troop.client/src/components/Event.jsx
@@ -9,10 +9,21 @@ function Event({ event }) {
 
     <div className="col-12 col-md-6 col-lg-4 col-xl-3 px-3 py-3">
       <Link to={`events/${event.id}`} title={`Visit the details page of "${event.name}"!`}>
-        <div className="event-card selectable">
-          <span className="text-dark">
-            Hi
-          </span>
+        <div className="event-card selectable" style={{ backgroundImage: `url(${event.coverImg})` }}>
+          <div className="w-100 px-1 d-flex flex-column bg-blur">
+            <span className="fs-5 text-light text-shadow">
+              {event.name}
+            </span>
+            <span className="text-light text-shadow">
+              {event.location}
+            </span>
+            <span className="text-light text-shadow">
+              {event.startDate}
+            </span>
+            <span className="text-light text-shadow text-end">
+              {event.capacity} spots left
+            </span>
+          </div>
         </div>
       </Link>
     </div>
